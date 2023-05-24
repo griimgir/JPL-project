7293aa5dc69a1a065b93e0428edddbcc2d335a2a
griimgir <griimgir@gmail.com>
2023-05-23 19:22:33 -0700

    Spacing and added Reset button

--- src/App.js
+++ src/App.js
@@ -34,42 +34,42 @@ function App() {
     //1970s terminal inspired mission control console interface
     <div className="App">
       <div>
-      <div className='head'>
-      <h1>>Created by: Alberc Ej Salcedo</h1>
-      </div>
-      <div className='head'>
-      <h1>></h1><h1 onClick={gitHandle}>Project Github Repo</h1><h1 className='blink2'>|</h1>
-      <span><button class='buttonRadius' onClick={() => window.location.reload(false)}>Reset</button></span>
-      </div>
-      <div className="Container">
+        <div className='head'>
+          <h1>>Created by: Alberc Ej Salcedo</h1>
+        </div>
+        <div className='head'>
+          <h1>></h1><h1 onClick={gitHandle}>Project Github Repo</h1><h1 className='blink2'>|</h1>
+          <span><button class='buttonRadius' onClick={() => window.location.reload(false)}>Reset</button></span>
+        </div>
+        <div className="Container">
           <div><h1>>start ./mission-control-sim<span className='blink'>|</span></h1></div>
           <br></br>
-        <h1 className="Drop">
-        <span className="Drop"><Dropmenu /></span>
-        <span className="Drop"><Dropmenu2 /></span>
-        </h1>
+          <h1 className="Drop">
+            <span className="Drop"><Dropmenu /></span>
+            <span className="Drop"><Dropmenu2 /></span>
+          </h1>
 
-        <h1 className="Space">Choose Time and Date for Launch</h1>
-        <h1><TimePicker disableClock={true} onChange={setTime} value={Time} />
-        <DatePicker onChange={setDate} value={thisDate} /></h1>
-        <h1>
-          <button onClick={start}>START LAUNCH SEQUENCE</button>
-          <p>{time > 0 ? 'T-Minus ' + time + ' seconds from Launch' : <span className='blink'>LIFT OFF</span>}</p>
-          {status === 'RUNNING'}
-        </h1>
-        <h1>
-          <span>Velocity: </span>
-          {time <= 0 ? <CountUp duration={120} end={24610} easingFn={easingFn} /> : ' - '}<span>MPH</span>
-        </h1>
-        <h1>
-          <span>Altitude: </span>
-          {time <= 0 ? <CountUp duration={120} end={120} /> : ' - '}<span> km</span>{/*28.5°*/}
-        </h1>
-        <h1>
-          <span>Fuel Burn: </span>
-          {time <= 0 ? <CountUp duration={120} end={3200} /> : ' - '}<span> lbs/s</span>
-        </h1>
-      </div>
+          <h1 className="Space">Choose Time and Date for Launch</h1>
+          <h1><TimePicker disableClock={true} onChange={setTime} value={Time} />
+            <DatePicker onChange={setDate} value={thisDate} /></h1>
+          <h1>
+            <button onClick={start}>START LAUNCH SEQUENCE</button>
+            <p>{time > 0 ? 'T-Minus ' + time + ' seconds from Launch' : <span className='blink'>LIFT OFF</span>}</p>
+            {status === 'RUNNING'}
+          </h1>
+          <h1>
+            <span>Velocity: </span>
+            {time <= 0 ? <CountUp duration={120} end={24610} easingFn={easingFn} /> : ' - '}<span>MPH</span>
+          </h1>
+          <h1>
+            <span>Altitude: </span>
+            {time <= 0 ? <CountUp duration={120} end={120} /> : ' - '}<span> km</span>{/*28.5°*/}
+          </h1>
+          <h1>
+            <span>Fuel Burn: </span>
+            {time <= 0 ? <CountUp duration={120} end={3200} /> : ' - '}<span> lbs/s</span>
+          </h1>
+        </div>
       </div>
     </div>
   );
